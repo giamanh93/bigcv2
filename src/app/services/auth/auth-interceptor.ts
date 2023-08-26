836aadee303a32c58c27141cf37e0d2a9f103241
Nguyễn Gia Mạnh <manhng@sunshinegroup.vn>
2023-08-26 14:21:03 +0700

    sssss

--- src/app/services/auth/auth-interceptor.ts
+++ src/app/services/auth/auth-interceptor.ts
@@ -1,54 +1,44 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { BehaviorSubject, Observable, throwError } from 'rxjs';
-import { catchError, filter, switchMap, take } from 'rxjs/operators';
-import { AuthService } from './auth.service';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpRequest,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgxSpinnerService} from 'ngx-spinner';
+import {MessageService} from 'primeng/api';
+import {BehaviorSubject, Observable, throwError} from 'rxjs';
+import {catchError, filter, switchMap, take} from 'rxjs/operators';
+import {AuthService} from './auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
   constructor(
     private auth: AuthService, private router: Router,
-    private spinner: NgxSpinnerService) { }
+    private messageService: MessageService,
+    private spinner: NgxSpinnerService) {
+  }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      const token = this.auth.getToken();
-
-     if (token) {
-       if (request.url.includes('https://media.bigcv.vn')) {
-         request = request.clone({
-           setHeaders: {
-              Authorization: `Bearer ${token}`,
-             'accept': '*/*',
-             // 'Content-Type': 'multipart/form-data'
-           }
-         });
-       } else {
-         request = request.clone({
-           setHeaders: {
-             Authorization: `Bearer ${token}`,
-           }
-         });
-       }
-
-    }
-
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.auth.getToken();
+    request = this.addAuthenticationToken(token, request);
     return next.handle(request).pipe(
-      catchError((err) => {
-        if (err instanceof HttpErrorResponse) {
-            if (err.status === 401) {
-            this.auth.logout();
-            location.reload();
-            this.spinner.hide();
-            } else if (err.status === 404 || err.status === 500 || err.status === 403) {
-              this.spinner.hide();
-            }
-      }
-      return throwError(err);
-    })
-     );
-    }
+      catchError((error: HttpErrorResponse) => {
+        if (error && error.status === 401) {
+          return this.handle401Error(request, next);
+        } else {
+          this.handleServerSideError(error);
+          return throwError(error);
+        }
+      })
+    ) as Observable<HttpEvent<any>>;
+  }
+
   private isRefreshing = false;
   private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
@@ -61,13 +51,14 @@ export class AuthInterceptor implements HttpInterceptor {
         return this.auth.refreshToken(refreshToken).pipe(
           switchMap((response: any) => {
             this.isRefreshing = false;
-            this.auth.loginSuccess(response.data);
-            this.refreshTokenSubject.next(response.data.access_token);
-            return next.handle(this.addAuthenticationToken(response.data.access_token, request));
+            this.auth.loginSuccess(response);
+            this.refreshTokenSubject.next(response.access_token);
+            return next.handle(this.addAuthenticationToken(response.access_token, request));
           }),
           catchError((err) => {
             this.isRefreshing = false;
             this.auth.logout();
+            this.router.navigateByUrl('/login');
             return throwError(err);
           })
         );
@@ -80,31 +71,33 @@ export class AuthInterceptor implements HttpInterceptor {
     );
   }
 
-  private addAuthenticationToken(token: string | null, request: HttpRequest<any>): HttpRequest<any> {
-    // request = request.clone({
-    //   setHeaders: { 'Content-Type': 'application/json' }
-    // });
-    if (token != null && !request.url.includes('admin/v1/loginAdmin')) {
-      const urls = request.url.split('?');
-      if (urls && urls.length > 0 && urls[0].includes('media/api/upload/')) {
-        request = request.clone(
-          {
-            headers: new HttpHeaders({
-              'accept': '*/*',
-            })
+  private addAuthenticationToken(token, request: HttpRequest<any>): HttpRequest<any> {
+    if (token) {
+      if (request.url.includes('https://media.bigcv.vn')) {
+        request = request.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`,
+            'accept': '*/*',
+            // 'Content-Type': 'multipart/form-data'
           }
-        );
+        });
+      } else if (request.url.includes('https://auth.bigcv.vn')) {
+        request = request.clone({
+          setHeaders: {
+            // Authorization: `Bearer ${token}`,
+            'accept': '*/*',
+            // 'Content-Type': 'multipart/form-data'
+          }
+        });
       } else {
         request = request.clone({
-          headers: new HttpHeaders({
-            'Authorization': 'Bearer ' + token,
-            'Content-Type': 'application/json'
-          })
+          setHeaders: {
+            Authorization: `Bearer ${token}`,
+          }
         });
       }
-
     }
-    console.log(request);
+
     return request;
   }
 
@@ -113,23 +106,29 @@ export class AuthInterceptor implements HttpInterceptor {
     console.log('Error', error.status);
     switch (error.status) {
       case 400:
-        // this.messageService.add({ severity: 'error', summary: 'Thông báo', detail: `Error: Bạn không có quyền sử dụng chức năng này` });
+        // this.messageService.add({severity: 'error', summary: 'Thông báo', detail: `Error: Bạn không có quyền sử dụng chức năng này`});
+        this.spinner.hide();
+        this.router.navigateByUrl('/login');
+        handled = true;
+        break;
+      case 401:
+        // this.messageService.add({severity: 'error', summary: 'Thông báo', detail: `Error: Bạn không có quyền sử dụng chức năng này`});
         this.spinner.hide();
         this.router.navigateByUrl('/login');
         handled = true;
         break;
       case 403:
-        // this.messageService.add({ severity: 'error', summary: 'Thông báo', detail: `Error: Bạn không có quyền sử dụng chức năng này` });
+        // this.messageService.add({severity: 'error', summary: 'Thông báo', detail: `Error: Bạn không có quyền sử dụng chức năng này`});
         this.spinner.hide();
         handled = true;
         break;
       case 500:
-        // this.messageService.add({ severity: 'error', summary: 'Thông báo', detail: `Error: Lỗi kết nối 500 !` });
+        // this.messageService.add({severity: 'error', summary: 'Thông báo', detail: `Error: Lỗi kết nối 500 !`});
         this.spinner.hide();
         handled = true;
         break;
       case 0:
-        // this.messageService.add({ severity: 'error', summary: 'Thông báo', detail: `Error: Lỗi kết nối service !` });
+        // this.messageService.add({severity: 'error', summary: 'Thông báo', detail: `Error: Lỗi kết nối service !`});
         this.spinner.hide();
         handled = true;
         break;
